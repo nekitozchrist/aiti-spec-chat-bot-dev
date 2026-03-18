d2133106c86b97c867049351c621fdfa27fe1d5e
Nine Measure <124411327+nekitozchrist@users.noreply.github.com>
2026-03-18 23:49:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 (function() {
 
-    const app_id = window.APP_ID;
+    const app_id = document.body.dataset.appId;
     
     if (!window.vkBridge) {
         document.getElementById('content').innerHTML = 
