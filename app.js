c8ec97c3c105bbdb0fbc6051ab04d9645143026e
Nine Measure <124411327+nekitozchrist@users.noreply.github.com>
2026-03-18 23:04:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@
     
     function initApp() {
         bridge.send("VKWebAppGetAuthToken", {
-            "app_id": window.APP_ID,
+            "app_id": ${{ vars.APP_ID$ }},
             "scope": ""
         }).then(function(data) {
             userToken = data.access_token;
